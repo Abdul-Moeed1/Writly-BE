d298776cc1746a890dc4d004644c4225e10e88db
Abdul-Moeed1 <abdulmoeedjhelumi@gmail.com>
2025-11-26 17:35:35 +0500

    Working on Login API

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -1,18 +1,26 @@
-import { Body, Controller, Get, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { createUserDTO } from './DTOs/createUser.dto';
+import { logInDTO } from './DTOs/logIn.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Get('login')
-  getHello() {
-    return 'user logged in';
-  }
-
   @Post('signup')
   createUser(@Body() createUserDTO: createUserDTO) {
     return this.authService.createUser(createUserDTO);
+  }
+
+  @Post('login')
+  login(@Body() logInDTO: logInDTO) {
+    return this.authService.logIn(logInDTO);
   }
 }
--- src/api/auth/auth.service.ts
+++ src/api/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { createUserDTO } from './DTOs/createUser.dto';
 import { User } from './models/user.model';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { logInDTO } from './DTOs/logIn.dto';
 
 @Injectable()
 export class AuthService {
@@ -13,5 +14,16 @@ export class AuthService {
   async createUser(payload: createUserDTO) {
     const user = new this.userModal(payload);
     return await user.save();
+  }
+
+  async logIn(payload: logInDTO) {
+    const user = await this.userModal.findOne({ email: payload.email });
+    if (!user) {
+      throw new BadRequestException('Invalid credentials');
+    }
+    if (user.password !== payload.password) {
+      throw new BadRequestException('Invalid credentials');
+    }
+    return user;
   }
 }
